7ed7cdd19908cb28d9e2c4786c95c0fb00faa60b
Devin Neal <devin@eulertour.com>
2025-02-03 01:34:53 -0500

    write file with state senator urls

--- index.ts
+++ index.ts
@@ -1,12 +1,58 @@
-import * as cheerio from 'cheerio';
-import axios from 'axios';
+import * as cheerio from "cheerio";
+import axios from "axios";
+import { promises as fs } from "fs";
 
-const html = await axios.get('https://www.nysenate.gov/senators-committees');
+const NEW_YORK_STATE_SENATE_WIKIPEDIA_URL =
+  "https://en.wikipedia.org/wiki/New_York_State_Senate";
 
+const createSenatorUrl = (href: string) => {
+  return "https://en.wikipedia.org" + href;
+};
 
+const html = await axios.get(NEW_YORK_STATE_SENATE_WIKIPEDIA_URL);
 
-const $ = cheerio.load(html.data);
+let $ = cheerio.load(html.data);
 
-const imgs = $('img[itok]').text();
+const memberTableData = $("h2#Current_members")
+  .parent()
+  .next()
+  .extract({
+    urlInfo: [
+      {
+        selector: "tbody tr",
+        value: {
+          name: "td:first-of-type a",
+          href: {
+            selector: "td:first-of-type a",
+            value: "href",
+          },
+        },
+      },
+    ],
+  });
 
-console.log(imgs);
+// console.log(memberTableData);
+
+const urlData = [];
+for (let { name, href } of memberTableData.urlInfo) {
+  if (name === undefined || href === undefined) {
+    continue;
+  }
+
+  const senatorUrl = createSenatorUrl(href);
+  let html = await axios.get(senatorUrl);
+
+  $ = cheerio.load(html.data);
+  const imageData = $("td.infobox-image").extract({
+    href: {
+      selector: "img",
+      value: "src",
+    },
+  });
+  const imageHref = imageData.href;
+  const imageUrl = "https:" + imageHref;
+
+  urlData.push({ name, imageUrl });
+}
+
+await fs.writeFile("data.json", JSON.stringify(urlData));
